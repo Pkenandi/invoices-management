3d672f79d54f2725e527554feb5df121e0374e1e
Pkenandi <kenandiprince@yahoo.com>
2023-08-04 16:22:39 +0200

    Add database configurations

---
+++ backend/src/main/java/io/magesty/invoicesmanagement/InvoicesmanagementApplication.java
@@ -0,0 +1,13 @@
+package io.magesty.invoicesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesmanagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/magesty/invoicesmanagement/InvoicesmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.magesty.invoicesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
